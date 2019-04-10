92c0518e745dc6857247ccc12d13304df71760be
Marcintt <marcin.wikarski@ttpsc.pl>
2019-04-10 16:07:19 +0200

    new

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
+        Zad1();
+        Zad2();
+        Zad3();
         System.out.println("Hello World!");
 
+
+
+
+
+
+    }
+
+    public static void Zad1() {
+        //1. W ramach testów zadeklaruj po jednej zmiennej typów: boolean, char, int, long, float, dobule, String,
+        // oraz wypisz je na konsolę.
         boolean zmiennaBoolean = true;
         char pojedynczyZnak = 'w';
         int liczba = 123;
@@ -11,11 +24,41 @@ public class Main {
         double najwiekszaLiczba = 123456.5;
         String newString = "example";
         System.out.println(pojedynczyZnak);
-        System.out.println(liczba)
-
+        System.out.println(liczba);
+        System.out.println(duzaLiczba);
+        System.out.println(innaLiczba);
+        System.out.println(najwiekszaLiczba);
+        System.out.println(newString);
     }
-    //1. W ramach testów zadeklaruj po jednej zmiennej typów: boolean, char, int, long, float, dobule, String,
-    // oraz wypisz je na konsolę.xxx
+
+    public static void Zad2(){
+        //2. Zadeklaruj dwie zmienne liczbowe, oraz wypisz na ekran wyniki kilku podstawowych działań które można na nich wykonać (+, -, *, /, %).
+
+        int liczba1 = 12;
+        int liczba2 = 5;
+        int dodawanie = liczba1 + liczba2;
+        int odejmowanie = liczba1 - liczba2;
+        double mnozenie = liczba1 * liczba2;
+        double dzielenie = liczba1 / liczba2;
+        double modulo = liczba1 % liczba2;
+
+        System.out.println("Działania:");
+        System.out.println(dodawanie);
+        System.out.println(odejmowanie);
+        System.out.println(mnozenie);
+        System.out.println(dzielenie);
+        System.out.println(modulo);
+    }
+    public static void Zad3(){
+        //3. Napisz program służący do konwersji wartości temperatury podanej w stopniach Celsjusza na stopnie w
+        // skali Fahrenheita oraz odwrotnie (stopnie Fahrenheita = 1.8 * stopnie Celsjusza + 32.0). Dane które mamy
+        // skonwertować umieszczamy na początku programu w zmiennych (np. double stopnieC, oraz double stopnieF).
+
+        double stopnieC = 100;
+        double stopnieF = 100;
+        stopnie1 =
+                stopnie2 =
+    }
 
 
 }

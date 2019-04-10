2612773d0c5f04187a303def163d76f2994fe932
Marcin Wikarski <wikarskim@tt.com.pl>
2019-04-10 15:22:54 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        boolean zmiennaBoolean = true;
+        char pojedynczyZnak = 'w';
+        int liczba = 123;
+        long duzaLiczba = 1234;
+        float innaLiczba = 12345.5f;
+        double najwiekszaLiczba = 123456.5;
+        String
+        System.out.println(pojedynczyZnak);
+        System.out.println(liczba);
+
+    }
+    //1. W ramach testów zadeklaruj po jednej zmiennej typów: boolean, char, int, long, float, dobule, String,
+    // oraz wypisz je na konsolę.
+
+
+}
+
+
